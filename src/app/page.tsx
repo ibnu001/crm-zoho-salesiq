09a69eead241b2c5cba8f7d74c41653c688e4110
ibnu <ibnu@mykonsul.com>
2025-07-01 14:47:52 +0700

    Refactor home page layout for improved styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 bg-gray-100">
+    <div className="flex flex-col  min-h-screen p-8 bg-gray-100">
       <h1 className="text-3xl font-bold">
         Welcome to Zoho SalesIQ with Next.js and Tailwind CSS
       </h1>
